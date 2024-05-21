00dc2e685cd74690d184b2c10eed6f880ee1cbbe
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-05-22 05:47:51 +0700

    feat: add regions controller with complete restfull function

---
+++ app/controllers/regions_controller.ts
@@ -0,0 +1,77 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import Region from '#models/region'
+import { responseUtil } from '../../helper/response_util.js'
+import vine, { SimpleMessagesProvider } from '@vinejs/vine'
+
+export default class RegionsController {
+  async index({ response }: HttpContext) {
+    const regions = await Region.all()
+    return responseUtil.success(response, regions, 'Regions retrieved successfully')
+  }
+
+  async show({ params, response }: HttpContext) {
+    const region = await Region.find(params.id)
+    if (!region) {
+      return responseUtil.notFound(response)
+    }
+    return responseUtil.success(response, region, 'Region retrieved successfully')
+  }
+
+  async store({ request, response }: HttpContext) {
+    const data = await vine
+      .compile(
+        vine.object({
+          name: vine.string().trim(),
+          zone_id: vine.number(),
+        })
+      )
+      .validate(request.all(), {
+        messagesProvider: new SimpleMessagesProvider({
+          'required': 'The {{ field }} field is required.',
+        }),
+      })
+
+    const region = await Region.create({
+      name: data.name,
+      zone_id: data.zone_id,
+    })
+
+    return responseUtil.created(response, region)
+  }
+
+  async update({ params, request, response }: HttpContext) {
+    const region = await Region.find(params.id)
+    if (!region) {
+      return responseUtil.notFound(response)
+    }
+
+    const data = await vine
+      .compile(
+        vine.object({
+          name: vine.string().trim(),
+          zone_id: vine.number(),
+        })
+      )
+      .validate(request.all(), {
+        messagesProvider: new SimpleMessagesProvider({
+          'required': 'The {{ field }} field is required.',
+        }),
+      })
+
+    region.name = data.name
+    region.zone_id = data.zone_id
+    await region.save()
+
+    return responseUtil.success(response, region, 'Region updated successfully')
+  }
+
+  async destroy({ params, response }: HttpContext) {
+    const region = await Region.find(params.id)
+    if (!region) {
+      return responseUtil.notFound(response)
+    }
+
+    await region.delete()
+    return responseUtil.noContent(response)
+  }
+}
